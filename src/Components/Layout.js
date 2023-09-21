3ba8c63db76baa5e5b73085b09b7e8adb750669e
Yogesh <91626383+yogesh087@users.noreply.github.com>
2023-09-22 03:30:47 +0530

    Update Layout.js

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header from "./Headers";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <div className="content">{children}</div>
+      <div className="content">{childrens}</div>
       <Footer />
     </>
   );
